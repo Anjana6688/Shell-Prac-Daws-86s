54631717f39fcdd353c5be1e0806d1c8b30c56e6
anjana <anjanarajasekhar06963@gmail.com>
2025-09-30 21:28:03 +0530

    variable with arugments

---
+++ 04-variable-arug.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+PERSON3=$3
+
+echo "$PERSON1 :: I'm Head of the family"
+echo "$PERSON2 :: I'm Servant for this family"
+echo "$PERSON3 :: I'm Princess for this family"
+echo "$PERSON1 says to $PERSON2 get lost"

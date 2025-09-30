88df31156543d870cab863ef224fa5a8d5e57205
anjana <anjanarajasekhar06963@gmail.com>
2025-09-30 21:20:21 +0530

    variable printing

---
+++ 03-Variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+PERSON1="SAIRAM"
+PERSON2="ANJANA"
+PERSON3="PRANAVI"
+
+echo "$PERSON1 :: I'm Head of the family"
+echo "$PERSON2 :: I'm Servant for this family"
+echo "$PERSON3 :: I'm Princess for this family"
+echo "$PERSON1 says to $PERSON2 get lost"

3d3b7f6ecfb56c1288fe0a8c22cdf0e01e0e0b44
anjana <anjanarajasekhar06963@gmail.com>
2025-10-01 21:53:36 +0530

    commit1

--- 05-var-password.sh
+++ 05-var-password.sh
@@ -6,4 +6,4 @@ echo "please enter username:"
 read -s USER_NAME # here USER_NAME is variable -s helpful to hide data from screen
 read -sp "please Enter Your Password:" PASSWORD #READ -P WILL HELPFUL to REMOVE extra ECHO COMMAND IT WILL Directly prints
 
-echo "entered $USER_NAME and $PASSWORD
+echo "entered $USER_NAME and $PASSWORD"S
